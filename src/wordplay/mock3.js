8f79e750ac92071158799fa2e8fe87888da7d0ec
johnsonthieu <johnsonle2929@gmail.com>
2022-04-04 18:17:50 -0700

    added prefixAndSuffix and mock3

---
+++ src/wordplay/mock3.js
@@ -0,0 +1,64 @@
+var fs = require('fs');
+var text = fs.readFileSync("./sowpods.txt", 'utf-8');
+var textByLine = text.split('\n')
+
+// What are all of the words that have only “E” as the vowel and are at least 15 letters?
+
+let vowelsWithoutE = ["A","I","O","U"]
+
+const getWordsWithOnlyEAnd15LettersOrLonger = (sowpods) =>{
+  let arr = [];
+  for(let i = 0; i < sowpods.length; i++){
+    if((sowpods[i].includes('E')) && (sowpods[i].length >= 15) && (noOtherVowelsButE(sowpods[i]))        
+    ){
+      arr.push(sowpods[i])
+    }
+  }
+  return arr
+}
+
+const noOtherVowelsButE = (word) => {
+  if(!word.includes("A") 
+    && !word.includes("I") 
+    && !word.includes("O") 
+    && !word.includes("U")) {
+      return true
+    }else{
+      return false
+    }        
+}
+
+
+// console.log(getWordsWithOnlyEAnd15LettersOrLonger(textByLine))
+
+// console.log(noOtherVowelsButE("BEEKEEPAR"))
+
+// - What are all of the words that start with “PRO”, end in “ING”, and are exactly 11 letters long?
+
+const checkProAndIng = (sowpods) => {
+  let arr = [];
+  for(let i = 0; i < sowpods.length; i++){
+    if(sowpods[i].startsWith("PRO") && sowpods[i].endsWith("ING")){
+      arr.push(sowpods[i])
+    }
+
+  }
+  return arr
+}
+
+// take output of checkProAndIng and sort ascending order 
+
+const getShortest = (sowpods) => {
+  let words = checkProAndIng(sowpods);
+  let shortest = words[0];
+  let shortestWords = [];
+  for(let i = 0; i < words.length; i++){
+    if(words[i].length <= shortest.length){
+      shortestWords.push(words[i])
+      shortest=words[i]
+    }
+  }
+return shortestWords
+}
+
+getShortest(textByLine)
---
+++ src/wordplay/prefixAndSuffix.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var text = fs.readFileSync("./sowpods.txt", 'utf-8');
+var textByLine = text.split('\n')
+
+
+// Write a function that takes a prefix and a suffix, and returns True if there are words with the prefix and suffix in sowpods.txt.
+
+// loop the sowpods and check if the prefix and suffix is included in sowpods
+// if so return 
+const checkPrefixAndSuffix = (prefix, suffix, sowpods) => {
+
+  let obj = {
+  };
+
+  let pre = [];
+  let suf = [];
+  for( let i = 0; i < sowpods.length; i++){
+
+    if(sowpods[i].startsWith(prefix)){      
+      pre.push(sowpods[i])
+    }
+    if(sowpods[i].endsWith(suffix)){
+      suf.push(sowpods[i]);
+    }
+  }
+  obj[prefix] = pre
+  obj[suffix] = suf
+  return obj
+}
+
+console.log('checkPrefixAndSuffix', checkPrefixAndSuffix('PRE', 'ATE', textByLine))
+// console.log('checkPrefixAndSuffix', checkPrefixAndSuffix('ANTI', 'TION', textByLine))
