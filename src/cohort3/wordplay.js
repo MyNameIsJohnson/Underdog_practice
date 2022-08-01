286fa46cf4772b95a7c54f7b8cb4a9ba0de03a69
johnsonthieu <johnsonle2929@gmail.com>
2022-07-31 19:19:13 -0700

    worked on crud

--- src/cohort3/wordplay.js
+++ src/cohort3/wordplay.js
@@ -1,18 +1,31 @@
 const fs = require("fs");
-const sowpods = fs.readFileSync("./sowpods.txt", "utf-8").split("\r\n");
+// for windows
+// const sowpods = fs.readFileSync("./sowpods.txt", "utf-8").split("\r\n");
+
+// for mac
+const sowpods = fs.readFileSync("./sowpods.txt", "utf-8").split("\n");
+const text = fs.readFileSync("./sowpods.txt", "utf-8");
+
 // [ ] What are all of the words containing UU?
 
-const getAllUUWords = (wordlist) => {
-  let allUUWords = [];
-  for (let i = 0; i < wordlist.length; i++) {
-    if (wordlist[i].includes("UU")) {
-      allUUWords.push(wordlist[i]);
-    }
+let allUUWords = [];
+for (let i = 0; i < sowpods.length; i++) {
+  if (sowpods[i].includes("UU")) {
+    allUUWords.push(sowpods[i]);
   }
-  return allUUWords;
-};
-console.log("getAllUUWords", getAllUUWords(sowpods));
+}
+// console.log(allUUWords);
+
 // [ ] What are all of the words containing an X and a Y and a Z?
+// Need to write a func that checks 3 conditionals. Loop through list checking each word and add to allWordsWithXYZ the word f it includes X,Y and Z.
+let allWordsWithXYZ = [];
+for (let word of sowpods) {
+  if (word.includes("X") && word.includes("Y") && word.includes("Z")) {
+    allWordsWithXYZ.push(word);
+  }
+}
+// console.log(allWordsWithXYZ);
+
 // [ ] What are all of the words containing a Q but not a U?
 // [ ] What are all of the words that contain the word CAT and are exactly 5 letters long?
 // [ ] What are all of the words that have no E or A and are at least 15 letters long?
@@ -27,7 +40,72 @@ console.log("getAllUUWords", getAllUUWords(sowpods));
 // [ ] How many words contain the substring "TYPE”?
 // [ ] Create and print an array containing all of the words that end in "GHTLY"
 // [ ] What is the shortest word that contains all 5 vowels?
+// loop through wordlist, check if word includes vowels, add word to list, sort list and return first index
+const getShortestWordWithAllVowels = (wordlist) => {
+  let list = [];
+  for (let word of wordlist) {
+    if (
+      word.includes("A") &&
+      word.includes("E") &&
+      word.includes("I") &&
+      word.includes("O") &&
+      word.includes("U")
+    ) {
+      list.push(word);
+    }
+  }
+  list.sort();
+  return list[0];
+};
+// console.log(getShortestWordWithAllVowels(sowpods));
+
 // [ ] What is the longest word that contains no vowels?
+const getLongestWordWithNoVowels = (wordlist) => {
+  let list = [];
+  for (let word of wordlist) {
+    if (
+      !word.includes("A") &&
+      !word.includes("E") &&
+      !word.includes("I") &&
+      !word.includes("O") &&
+      !word.includes("U")
+    ) {
+      list.push(word);
+    }
+  }
+  list.sort((a, b) => {
+    return b.length - a.length;
+  });
+  return list[0];
+};
+console.log(getLongestWordWithNoVowels(sowpods));
+
 // [ ] Which of the letters Q, X, and Z is the least common?
+
+function leastCommonLetter(str) {
+  var q = 0;
+  var x = 0;
+  var z = 0;
+  var arr = {};
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === "Q") {
+      q++;
+      arr[str[i]] = q;
+    }
+    if (str[i] === "X") {
+      x++;
+      arr[str[i]] = x;
+    }
+    if (str[i] === "Z") {
+      z++;
+      arr[str[i]] = z;
+    }
+  }
+
+  let min = Math.min(...Object.values(arr));
+  return Object.keys(arr).filter((key) => arr[key] == min);
+}
+
+console.log(leastCommonLetter(text));
 // [ ] What is the longest palindrome?
 // [ ] What are all of the letters that never appear consecutively in an English word? For example, we know that “U” isn’t an answer, because of the word VACUUM, and we know that “A” isn’t an answer, because of “AARDVARK”, but which letters never appear consecutively?
--- src/crudExercises.js
+++ src/crudExercises.js
@@ -7,7 +7,7 @@
 // - First, create an empty array called fruitsArr
 let fruitArr = [];
 // - Now, append the array with 5 fruits (ex: "apple", "orange", etc)
-fruitArr.push('apple', 'orange', 'pineapple', 'strawberry', 'banana');
+fruitArr.push("apple", "orange", "pineapple", "strawberry", "banana");
 
 // READ: now that we have our fruits array, we want to be able to read from it
 // to learn about what's in it
@@ -26,15 +26,15 @@ fruitArr.push('apple', 'orange', 'pineapple', 'strawberry', 'banana');
 // existing item in the array
 
 // - Now, change the first item in the array to be "mango"
-fruitArr[0] = 'mango'
+fruitArr[0] = "mango";
 // - Now, append the fruit "calamansi" to the fruits array (appending something
 //   is a form of updating our existing array)
-fruitArr.push('calamansi')
+fruitArr.push("calamansi");
 
 // DELETE: you can also delete things from an array
 // - Delete the last item in the array
-fruitArr.pop()
-console.log(fruitArr)
+fruitArr.pop();
+// console.log(fruitArr);
 
 // --------------------------------------------------------------------------
 // PROBLEM SET 2: DICTIONARIES (aka javascript objects)
@@ -49,43 +49,43 @@ console.log(fruitArr)
 //      - "grapes" which costs 3.24
 //      - "mango" which costs 2.20
 
-
 // READ
 // - Iterate over the dictionary with a for loop and print each key (fruit name)
 // - Iterate over the dictionary with a for loop and print each value (cost)
 // - Iterate over the dictionary with a for loop and print each key (fruit name) and value (cost)
 
-
 // UPDATE
 // - Add a new key and value to the fruits dictionary, where "kiwi" is the key
 //   and 5.13 is the value.
-
-
 
 // DELETE
 // - Print the dictionary first. Then, delete the key for "kiwi" that you added
 //   in the previous step. Then, print the dictionary again. Is the key for
 //   "kiwi" and its value of 5.13 gone? Did we delete it successfully?
 
-
 // --------------------------------------------------------------------------
 // PROBLEM SET 3: ITERATING OVER THE FRUITS DICTIONARY
 // Using the fruitsDict from the previous problem, can you tell me the total
 // cost of all fruits in the dictionary?
+//      - "apple" which costs 0.50
+//      - "pear" which costs 1.14
+//      - "orange" which costs 1.23
+//      - "grapes" which costs 3.24
+//      - "mango" which costs 2.20
 let fruitsDict = {
-  apple: 1.59,
-  orange: 2.23,
-  strawberry: 5.72,
-  banana: 0.37,
-}
-let total = 0
-for(let value in fruitsDict){
-  total += fruitsDict[value]
+  apple: 0.5,
+  pear: 1.14,
+  orange: 1.23,
+  grapes: 3.24,
+  mango: 2.2,
+};
+let total = 0;
+for (let value in fruitsDict) {
+  total += fruitsDict[value];
 }
 
-// figure how to return decimals 
-console.log(Math.ceil(total, 2))
-
+// figure how to return decimals
+// console.log(total);
 
 // --------------------------------------------------------------------------
 
@@ -93,43 +93,42 @@ console.log(Math.ceil(total, 2))
 // PROBLEM SET 4: CHECKING FOR ITEMS IN YOUR FRUITS DICTIONARY
 // A customer sends a message to your online fruit store. They send an array of
 // fruits that they want to buy:
-//      order = ["mango", "orange"]
+let order = ["mango", "orange"];
 // Using the array they sent and your fruitsDict, can you calculate the total
 // cost of the fruits that they want to buy?
+let cost = 0;
 
+for (let item of order) {
+  cost += fruitsDict[item];
+}
+
+// console.log(cost);
 
 // A customer sends a message to your online fruit store. They send an array of
 // fruits that they want to buy:
-//      order = ["mango", "strawberry"]
+order = ["mango", "strawberry"];
 // But oh no! Some of the fruits in their order are not in your fruitsDict. Can
 // you find the fruits that are unavailable (not in your fruits dict) and let
 // the customer know which ones they are?
 
-
+// for (let item of order) {
+//   if (fruitsDict[item]) {
+//     console.log("We have " + item);
+//   } else {
+//     console.log("We do not have " + item);
+//   }
+// }
 // Now, the customer has sent both the fruit names and the quantities that they
 // would like in the form of an order dictionary:
-//      orderDict = {
-//          "mango": 2,
-//          "orange": 5
-//      }
+let orderDict = {
+  mango: 2,
+  orange: 5,
+};
 // In the orderDict, they are telling you they want 2 mangoes and 5 oranges.
 // Using the dictionary they sent and your fruitsDict, can you calculate the total
 // cost of the fruits that they want to buy?
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let orderTotal = 0;
+for (let item in orderDict) {
+  orderTotal += fruitsDict[item] * orderDict[item];
+}
+console.log(orderTotal);
