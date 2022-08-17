c0f965443b80efed2d18f5ba20795438479856c9
johnson <johnsonle2929@gmail.com>
2022-08-17 09:25:33 -0700

    moved files

--- src/wordplay/test.py
+++ src/wordplay/test.py
@@ -1,35 +1,36 @@
-constList = [
-    'BALL',
-    'BEACH',
-    'MAN',
-    'KEEPER',
-    'SNOW',
-    'SNOWMAN'
-    'RAN',
-    'ZOO',
-    'ZOOKEEPER'
-]
+# constList = [
+#     'BALL',
+#     'BEACH',
+#     'MAN',
+#     'KEEPER',
+#     'SNOW',
+#     'SNOWMAN'
+#     'RAN',
+#     'ZOO',
+#     'ZOOKEEPER'
+# ]
 
-memoObject = {}
-answerArray = []
+# memoObject = {}
+# answerArray = []
 
-#Here we build the memoization object
-for word in constList:
-    memoObject[word] = word
+# #Here we build the memoization object
+# for word in constList:
+#     memoObject[word] = word
 
-#Loop Through the Word List
-for word in constList:
-    #Loop through the letters of each word and keep track of our index
-    for i in range(len(word)):
-        #Is the 0th through i in our memo object? 
-        if word[0:i] not in memoObject:
-            continue
-        #Here we find it
-        else:
-            #Now we check if the second part of the word is in the list
-            if word[i:] not in memoObject:
-                continue
-            #Here we find it and add it to an answer array.
-            else:
-                answerArray.append(word)
-print(answerArray)
+# #Loop Through the Word List
+# for word in constList:
+#     #Loop through the letters of each word and keep track of our index
+#     for i in range(len(word)):
+#         #Is the 0th through i in our memo object? 
+#         if word[0:i] not in memoObject:
+#             continue
+#         #Here we find it
+#         else:
+#             #Now we check if the second part of the word is in the list
+#             if word[i:] not in memoObject:
+#                 continue
+#             #Here we find it and add it to an answer array.
+#             else:
+#                 answerArray.append(word)
+# print(answerArray)
+
